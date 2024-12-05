a9e08e4cbf135d7120180490b298a95c867534fc
ardhiqii <ardhiqi@gmail.com>
2024-12-05 12:01:57 +0700

    section-14 end

--- animations-01-starting-setup/src/App.vue
+++ animations-01-starting-setup/src/App.vue
@@ -1,28 +1,82 @@
 <template>
-  <div class="container">
-    <div class="block"></div>
-    <button>Animate</button>
-  </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
-    <p>This is a test dialog!</p>
-    <button @click="hideDialog">Close it!</button>
-  </base-modal>
-  <div class="container">
-    <button @click="showDialog">Show Dialog</button>
-  </div>
-</template>  
+  <router-view v-slot="slotProps">
+    <transition name="fade-button" mode="out-in"> 
+      <component :is="slotProps.Component"></component>
+    </transition>
+  </router-view>
+</template>
 
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false };
+    return {
+      dialogIsVisible: false,
+      animatedBlock: false,
+      paraIsVisible: false,
+      usersAreVisible: false,
+      enterInterval: null,
+      leaveInterval: null,
+    };
   },
   methods: {
+    animateBlock() {
+      this.animatedBlock = true;
+    },
+    toggleParagraph() {
+      this.paraIsVisible = !this.paraIsVisible;
+    },
+
     showDialog() {
       this.dialogIsVisible = true;
     },
     hideDialog() {
       this.dialogIsVisible = false;
+    },
+    showUsers() {
+      this.usersAreVisible = true;
+    },
+    hideUsers() {
+      this.usersAreVisible = false;
+    },
+
+    enterCancelled() {
+      clearInterval(this.enterInterval);
+    },
+
+    leaveCancelled() {
+      clearInterval(this.leaveInterval);
+    },
+
+    beforeEnter(el) {
+      el.style.opacity = 0;
+    },
+
+    enter(el, done) {
+      let round = 1;
+      this.enterInterval = setInterval(() => {
+        el.style.opacity = round * 0.1;
+        round++;
+        if (round > 10) {
+          clearInterval(this.enterInterval);
+          done();
+        }
+      }, 20);
+    },
+
+    beforeLeave(el) {
+      el.style.opacity = 1;
+    },
+
+    leave(el, done) {
+      let round = 1;
+      this.leaveInterval = setInterval(() => {
+        el.style.opacity = 1 - round * 0.1;
+        round++;
+        if (round > 10) {
+          clearInterval(this.leaveInterval);
+          done();
+        }
+      }, 20);
     },
   },
 };
@@ -57,6 +111,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
+  /* transition: transform ease 0.3s; */
 }
 .container {
   max-width: 40rem;
@@ -68,5 +123,64 @@ button:active {
   padding: 2rem;
   border: 2px solid #ccc;
   border-radius: 12px;
+}
+.animate {
+  /* transform: translateX(-150px); */
+  animation: slide-fade 0.3s ease-out forwards;
+}
+
+.fade-button-enter-from,
+.fade-button-leave-to {
+  opacity: 0;
+}
+
+.fade-button-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+
+.fade-button-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+
+.fade-button-enter-to,
+.fade-button-leave-from {
+  opacity: 1;
+}
+
+.route-enter-from,
+.route-enter-active {
+  animation: slide-scale 0.4s ease-out;
+}
+
+.route-enter-to {
+}
+
+.route-leave-active {
+  animation: slide-scale 0.4s ease-in;
+}
+
+@keyframes slide-scale {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+  70% {
+    transform: translateX(-120px) scale(1.1);
+  }
+
+  100% {
+    transform: translateX(-150px) scale(1);
+  }
+}
+
+@keyframes modal {
+  from {
+    opacity: 0;
+    transform: translateY(-50px) scale(0.9);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateY(0) scale(1);
+  }
 }
 </style>
---
+++ animations-01-starting-setup/src/components/UsersList.vue
@@ -0,0 +1,78 @@
+<template>
+  <transition-group tag="ul" name="users-list">
+    <li v-for="user in users" :key="user" @click="removeUser(user)">
+      {{ user }}
+    </li>
+  </transition-group>
+
+  <div>
+    <input type="text" ref="userNameInput" />
+    <button @click="addUser">Add User</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: ['Max', 'Manu', 'Julie', 'Angela', 'Aufa'],
+    };
+  },
+  methods: {
+    addUser() {
+      const enteredUserName = this.$refs.userNameInput.value;
+      this.users.unshift(enteredUserName);
+      this.$refs.userNameInput.value = null;
+    },
+    removeUser(user) {
+      this.users = this.users.filter((u) => u !== user);
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 1rem 0;
+  padding: 0;
+}
+
+li {
+  border: 1px solid #ccc;
+  padding: 1rem;
+  text-align: center;
+}
+
+.users-list-enter-from {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+
+.users-list-enter-active {
+  transition: all 1s ease;
+}
+.users-list-enter-to {
+  opacity: 1;
+  transform: translateX(0px);
+}
+
+.users-list-leave-from {
+  opacity: 1;
+  transform: translateX(0);
+}
+
+.users-list-leave-active {
+  transition: all 1s ease;
+  position: absolute
+}
+
+.users-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+.users-list-move{
+    transition: transform 0.8s ease ;
+}
+</style>

6596daf126e755a78c977a9cdd1109ad45c68a6c
Aufa Fauqi Ardhiqi <ardhiqi@gmail.com>
2025-07-14 16:44:48 +0700

    auth page

--- main-prj-01-starting-setup/src/store/modules/requests/actions.js
+++ main-prj-01-starting-setup/src/store/modules/requests/actions.js
@@ -5,44 +5,48 @@ export default {
       message: payload.message,
     };
 
-    const res =  await fetch(`https://vue-http-demo-b2da9-default-rtdb.asia-southeast1.firebasedatabase.app/request/${payload.coachId}.json`,{
-      method: 'POST',
-      body: JSON.stringify(newRequest)
-    })
+    const res = await fetch(
+      `https://vue-http-demo-b2da9-default-rtdb.asia-southeast1.firebasedatabase.app/requests/${payload.coachId}.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest),
+      }
+    );
 
-    const resData = await res.json()
+    const resData = await res.json();
 
-    if(!res.ok){
-      const error = new Error(resData.message || 'Failed to send request.')
-      throw error
+    if (!res.ok) {
+      const error = new Error(resData.message || 'Failed to send request.');
+      throw error;
     }
 
-    newRequest.id = resData.name
-    newRequest.coachId = payload.coachId
+    newRequest.id = resData.name;
+    newRequest.coachId = payload.coachId;
     context.commit('addRequest', newRequest);
   },
-  async fetchRequests(context){
-    const coachId = context.rootGetters.userId
-    const res = await fetch(`https://vue-http-demo-b2da9-default-rtdb.asia-southeast1.firebasedatabase.app/request/${coachId}.json`)
-    const resData = await res.json()
+  async fetchRequests(context) {
+    const coachId = context.rootGetters.userId;
+    const res = await fetch(
+      `https://vue-http-demo-b2da9-default-rtdb.asia-southeast1.firebasedatabase.app/requests/${coachId}.json`
+    );
+    const resData = await res.json();
 
-    if(!res.ok){
-      const error = new Error(res.message || 'Faield to fetch requests.')
-      throw error
+    if (!res.ok) {
+      const error = new Error(res.message || 'Faield to fetch requests.');
+      throw error;
     }
 
-
-    const requests = []
-    for (const key in resData){
+    const requests = [];
+    for (const key in resData) {
       const request = {
         id: key,
         coachId: coachId,
         userEmail: resData[key].userEmail,
-        message: resData[key].message
-      }
-      requests.push(request)
+        message: resData[key].message,
+      };
+      requests.push(request);
     }
 
-    context.commit('setRequests',requests)
-  }
+    context.commit('setRequests', requests);
+  },
 };
